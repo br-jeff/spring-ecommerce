c5309054ba6d5bae97141cc9502eb4e0552a1252
jefferson silva <jefferson_1996Th@hotmail.com>
2024-09-08 10:12:15 -0300

    start of categoru model

---
+++ src/main/java/br/com/jeff/ecommerce/model/Category.java
@@ -0,0 +1,12 @@
+package br.com.jeff.ecommerce.model;
+
+import java.util.List;
+
+/**
+ * Category
+ */
+public class Category {
+  private Long id;
+  private String name;
+  private List<Product> products;
+}
