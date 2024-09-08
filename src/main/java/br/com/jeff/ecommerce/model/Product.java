78ad8d48157720d1048a2bddda37767c215a6318
jefferson silva <jefferson_1996Th@hotmail.com>
2024-09-08 10:14:20 -0300

    start of product model

---
+++ src/main/java/br/com/jeff/ecommerce/model/Product.java
@@ -0,0 +1,12 @@
+package br.com.jeff.ecommerce.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Product {
+  private Long id;
+  private String name;
+  private String description;
+  private BigDecimal pirce;
+  private int inventory;
+}
